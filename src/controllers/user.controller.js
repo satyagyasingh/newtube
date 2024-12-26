6379b210bd61cfe2a57fd4caade4b40b5fde0387
satyagyasingh <satyagyasingh@gmail.com>
2024-12-26 23:54:23 +0530

    validating requests

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,37 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js";
+
 
 const registerUser = asyncHandler(async (req,res) => {
+    
+    // get user details from frontend
+    // validation — not empty
+    // check if user already exists: username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object
+    // create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
+    
     console.log(req.body);
     
     const {fullName, email ,username, password} = req.body;
     console.log("email : ",username);
+
+    // if(fullName === ""){
+    //     throw new ApiError(400, "Fullname is required")
+    // }
+
+    if(
+        [fullName,email,username, password].some((feild) =>
+        feild?.trim() === "")//return -s true if any of feild is empty
+    ){
+        throw new ApiError(400,"All feilds are required")
+    }
+
+    
 
     return res.status(200).json({
         data : req.body,
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,21 @@
 import { Router } from "express";
 import registerUser from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router(); // Correctly create an instance of Router
 
-router.route("/register").post(registerUser); // Use the instance 'route'
+
+
+router.route("/register").post(
+    upload.fields([
+        {
+            name : "avatar",
+            maxCount : 1,
+        },
+        {
+            name : "coverImage",
+            maxCount : 1,
+        }
+    ]),
+    registerUser); // Use the instance 'route'
 export default router;
