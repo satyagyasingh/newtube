f6cb719399e985361de179d616a8c702109cef08
satyagyasingh <satyagyasingh@gmail.com>
2025-01-18 23:17:27 +0530

    methods to update accountdetails, avatar and coverImage

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -262,6 +262,95 @@ const getCurrentUSer = asyncHandler(async(req,res)=>{
 })
 
 
+const updateAccountDetails = asyncHandler(async(req,res) => {
+  const {fullName, email} = req.body
+  if(!fullName || !email){
+    throw new ApiError(400, "All  feilds are required")
+  }
+
+  const user = User.findByIdAndUpdate(
+    req.user?. _id,
+    {
+      $set :{
+        fullName,
+        email : email
+      }
+    },
+    {new : true}
+  ).select("-password")
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200,user , "Account details updated successfully"))
+
+})
+
+const updateUserAvatar = asyncHandler(async(req, res)=>{
+  const avatarLocalPath = req.file?.path
+
+  if(!avatarLocalPath){
+    throw new ApiError(400, "Avatar file is required")
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+  if(!avatar.url){
+    throw new ApiError(400, "Error while uploading on cloudinary")
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set : {
+        avatar : avatar.url
+      }
+    },
+    {new : true}
+  )
+
+  return res
+  .staus(200)
+  .json(
+    new ApiResponse(200, user, "avatar updated successfully")
+  )
+
+})
+
+
+
+const updateUserCoverImage = asyncHandler(async(req, res)=>{
+  const coverImageLocalPath = req.file?.path
+
+  if(!coverImageLocalPath){
+    throw new ApiError(400, "coverImage file is required")
+  }
+
+  
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+  if(!coverImage.url){
+    throw new ApiError(400, "Error while uploading on cloudinary")
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set : {
+        coverImage : coverImage.url
+      }
+    },
+    {new : true}
+  )
+
+  return res
+  .staus(200)
+  .json(
+    new ApiResponse(200, user, "CoverImage updated successfully")
+  )
+
+
+})
+
 
 export {
   registerUser,
@@ -269,7 +358,10 @@ export {
   logOutUser,
   refreshAccessToken,
   getCurrentUSer,
-  changeCurrentPassword
+  changeCurrentPassword,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage
 };
 
 
